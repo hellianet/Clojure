dee252d0329278987936a17c2015b7a15b752342
Kristina Lanchukovskaya <k.lanchukovskaya@azoft.com>
2022-12-12 00:48:55 +0700

    add task 1.3 - 1.4 and rework task 1.1 - 1.2

--- Lab1/task1_2.clj
+++ Lab1/task1_2.clj
@@ -1,9 +1,9 @@
 (defn generateWord [alphabet, word, result]
   (if (empty? alphabet)
     result
-     (if (.startsWith word (first alphabet))
+     (if (= (first alphabet) (first word))
       (recur (rest alphabet) word result)
-      (recur (rest alphabet) word (cons (str (first alphabet) word) result))
+      (recur (rest alphabet) word (cons (cons (first alphabet) word) result))
     )
   )
 )
@@ -16,14 +16,14 @@
 )
 
 (defn generate [alphabet, wordsSequence, n]
-  (if (= n 1)
+  (if (= n 0)
     wordsSequence
     (recur alphabet (generateWords alphabet wordsSequence '()) (- n 1))
   )
 )
 
 (defn wordGeneratorFactory [n]
-  (fn [alphabet] (generate alphabet, alphabet, n)))
+  (fn [alphabet] (generate alphabet, '(()), n)))
 
 (let [wordGenerator3 (wordGeneratorFactory 3)]
-  (println (wordGenerator3 '("a" "b" "c"))))
+  (println (wordGenerator3 '(a b c d))))
